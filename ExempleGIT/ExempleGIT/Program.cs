859014da94aeef5557d49809189efd6e064eaa2c
Simon Henri <simon.henri16@hotmail.com>
2017-02-01 10:28:08 -0500

    J'ai ajouté un commentaire.

--- ExempleGIT/ExempleGIT/Program.cs
+++ ExempleGIT/ExempleGIT/Program.cs
@@ -2,17 +2,14 @@ using System;
 
 namespace ExempleGIT
 {
-#if WINDOWS || XBOX
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         static void Main(string[] args)
         {
-                Console.WriteLine("Hi");
+            // fgasdfasdkfjagdkhfgdjkgfsdhgfasdhgfahksdgfkahgdsfjk
+                System.Console.WriteLine("Hi");
         }
     }
-#endif
 }
 
